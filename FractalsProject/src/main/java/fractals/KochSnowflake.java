866da55e43db89e504d5ae6614fdd45f16640fc6
JaroDomcio <146334892+JaroDomcio@users.noreply.github.com>
2024-06-19 16:39:54 +0200

    Updated fractals, fixed bugs

--- FractalsProject/src/main/java/fractals/KochSnowflake.java
+++ FractalsProject/src/main/java/fractals/KochSnowflake.java
@@ -2,26 +2,69 @@ package fractals;
 
 import java.awt.*;
 
-public class KochSnowflake extends Fractal{
+public class KochSnowflake extends Fractal {
     public KochSnowflake(int width, int height) {
         super(width, height);
-        this.maxIterations = 10;
+        this.maxIterations = 5;
+        this.zoom = 0.8;
         generateFractal();
     }
+
     public KochSnowflake(int width, int height, int maxIterations) {
         super(width, height);
         this.maxIterations = maxIterations;
         generateFractal();
     }
 
-
     @Override
     public void generateFractal() {
+        Graphics2D g2d = image.createGraphics();
+        g2d.setColor(Color.BLACK);
+        g2d.fillRect(0, 0, width, height);
+        g2d.setColor(Color.WHITE);
 
+        double size = Math.min(width, height) * zoom;
+        double x1 = width / 2 - size / 2 - offsetX;
+        double y1 = height / 2 + size / (2 * Math.sqrt(3)) - offsetY;
+        double x2 = width / 2 + size / 2 - offsetX;
+        double y2 = y1;
+        double x3 = width / 2 - offsetX;
+        double y3 = height / 2 - size / Math.sqrt(3) - offsetY;
+
+        drawKochSnowflake(g2d, maxIterations, x1, y1, x2, y2);
+        drawKochSnowflake(g2d, maxIterations, x2, y2, x3, y3);
+        drawKochSnowflake(g2d, maxIterations, x3, y3, x1, y1);
+
+        g2d.dispose();
+    }
+
+    private void drawKochSnowflake(Graphics2D g2d, int iterations, double x1, double y1, double x2, double y2) {
+        if (iterations == 0) {
+            g2d.drawLine((int) x1, (int) y1, (int) x2, (int) y2);
+        } else {
+            double deltaX = x2 - x1;
+            double deltaY = y2 - y1;
+
+            double xA = x1 + deltaX / 3;
+            double yA = y1 + deltaY / 3;
+
+            double xB = x1 + 2 * deltaX / 3;
+            double yB = y1 + 2 * deltaY / 3;
+
+            double xC = (x1 + x2) / 2 - Math.sqrt(3) * (y1 - y2) / 6;
+            double yC = (y1 + y2) / 2 + Math.sqrt(3) * (x1 - x2) / 6;
+
+            drawKochSnowflake(g2d, iterations - 1, x1, y1, xA, yA);
+            drawKochSnowflake(g2d, iterations - 1, xA, yA, xC, yC);
+            drawKochSnowflake(g2d, iterations - 1, xC, yC, xB, yB);
+            drawKochSnowflake(g2d, iterations - 1, xB, yB, x2, y2);
+        }
     }
 
     @Override
     protected void paintComponent(Graphics g) {
         super.paintComponent(g);
+        g.drawImage(image, 0, 0, this);
     }
 }
+
--- FractalsProject/src/main/java/fractals/SierpinskiTriangle.java
+++ FractalsProject/src/main/java/fractals/SierpinskiTriangle.java
@@ -2,7 +2,7 @@ package fractals;
 
 import java.awt.*;
 import java.awt.geom.Point2D;
-import java.awt.image.BufferedImage;
+
 
 public class SierpinskiTriangle extends Fractal {
 
@@ -15,7 +15,7 @@ public class SierpinskiTriangle extends Fractal {
 
     public SierpinskiTriangle(int width, int height) {
         super(width, height);
-        this.maxIterations = 7; // Domyślna wartość maxIterations
+        this.maxIterations = 7;
         this.zoom=1;
         generateFractal();
     }
@@ -23,26 +23,26 @@ public class SierpinskiTriangle extends Fractal {
     @Override
     public void generateFractal() {
         Graphics2D g = image.createGraphics();
-        g.setColor(Color.WHITE);
-        g.fillRect(0, 0, width, height);
         g.setColor(Color.BLACK);
+        g.fillRect(0, 0, width, height);
+        g.setColor(Color.WHITE);
 
-        double size = Math.min(width, height) *zoom;
+        double size = Math.min(width, height) * zoom;
 
         Point2D.Double p1 = new Point2D.Double(width / 2.0, (height - size * Math.sqrt(3) / 2) / 2.0);
         Point2D.Double p2 = new Point2D.Double((width - size) / 2.0, (height + size * Math.sqrt(3) / 2) / 2.0);
         Point2D.Double p3 = new Point2D.Double((width + size) / 2.0, (height + size * Math.sqrt(3) / 2) / 2.0);
 
-        p1.x += offsetX;
-        p1.y += offsetY;
-        p2.x += offsetX;
-        p2.y += offsetY;
-        p3.x += offsetX;
-        p3.y += offsetY;
+        p1.x -= offsetX;
+        p1.y -= offsetY;
+        p2.x -= offsetX;
+        p2.y -= offsetY;
+        p3.x -= offsetX;
+        p3.y -= offsetY;
 
         generateSierpinskiTriangle(g, maxIterations, p1, p2, p3);
 
-        g.dispose();
+
     }
 
     private void generateSierpinskiTriangle(Graphics2D g, int depth, Point2D.Double p1, Point2D.Double p2, Point2D.Double p3) {
